f8842bfd4c02b40bb0af1f7b1e8f59c8d1d183bc
ekarat <pokekarat@gmail.com>
2019-09-24 16:40:28 +0700

    bullets and enemies

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Rigidbody2D enemy;
+    List<Rigidbody2D> eList = new List<Rigidbody2D>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Vector3 ePos = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-5.0f, 5.0f),0);
+            Rigidbody2D e = Instantiate(enemy, ePos, transform.rotation) as Rigidbody2D;
+            e.tag = "enemy";
+            eList.Add(e);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         foreach (Rigidbody2D e in eList.ToArray())
+        {
+            if (e != null)
+            {
+               
+            }
+        }
+    }
+}
